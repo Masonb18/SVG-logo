b0122e7f1ec02cf51441f2fcc58e4497f27c10c2
Masonb18 <bauer.mason01@gmail.com>
2023-05-05 15:57:04 -0500

    added svg and the first question with a validate if statement

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { SVG } = require('@svgdotjs/svg.js')
+
+
+const questions = [
+    {
+        type: 'input',
+        name: 'character',
+        message: 'Enter up to three characters',
+
+        validate: (value) => {
+            if (value.length <= 3) {
+                return true;
+            } else {
+                return 'Enter 3 characters or less';
+            }
+        }
+    }
+]
